998035ec9a69a59ec687bd3e750ccd43eabe2ca0
johan <johan.chandra.putra@gmail.com>
2021-11-05 21:08:26 +0700

    postfixed

--- web/user/user_web.js
+++ web/user/user_web.js
@@ -14,17 +14,17 @@ function userSet(user) {
     };
 }
 
-// async function createUser(user) {
-//     try {
-//         const users = await models.users.create({
-//             username : user.username,
-//             password : user.password,
-//         });
-//         return userSet(users); 
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
+async function createUser(user) {
+    try {
+        const users = await models.users.create({
+            username : user.username,
+            password : user.password,
+        });
+        return userSet(users); 
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 router.get("/", async (req, res) => {
     const users = await models.users.findAll();
